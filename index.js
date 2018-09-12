198c42176f30eba755257cb30951095c55525c2d
Alexander Frosell <sachafrosell@gmail.com>
2018-09-12 20:54:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ function nowServing(line) {
     return "Currently serving " + line.shift();
   }
 }
+
+function currentLine(line){
+  if(line.length === 0) {
+    return "The line is currently empty.";
+  }
+  var lineNamesandNumbers = [];
+  
+  for(var i = 0; i < line.length; i++) {
+    lineNamesandNumbers.push(i+1 + ". "+ line[i]);
+  }
+  console.log("The line is currently: " + lineNamesandNumbers)
+  return "The line is currently: " + lineNamesandNumbers.join(', ');
+}
