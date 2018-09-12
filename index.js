540dbb445db1b7fd4a773ca38ad0b31fd4245308
Alexander Frosell <sachafrosell@gmail.com>
2018-09-12 20:41:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function takeANumber(currentLine, name) {
+  for (let i = 1; i <= currentLine.length; i++) {
+    if (currentLine[i] == name) {
+      return `Welcome, ${name}. You are number ${i} in Line.`
+    }
+  }
+}
+
 function 
