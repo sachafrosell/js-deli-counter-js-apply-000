bfec4fa5066f07ce398c89bc1836bf2c3f761d68
Alexander Frosell <sachafrosell@gmail.com>
2018-09-12 20:54:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ function takeANumber(currentLine, name) {
   return `Welcome, ${name}. You are number ${currentLine.length} in line.`
 }
 
-function nowServing()
+function nowServing(line) {
+  if(line.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+   
+    return "Currently serving " + line.shift();
+  }
+}
